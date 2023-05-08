8a727e8309a192d40510e5e0d06b7aae4456208d
niklas <niklas@sunway.ch>
2023-05-08 10:01:31 +0200

    ⚙ main loop

--- src/jfk.sh
+++ src/jfk.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/sh
+#!bin/bash
 
 echo "    | |  ___| |/ /"
 echo " _  | | |_  | ' / "
@@ -6,12 +6,31 @@ echo "| |_| |  _| | . \\ "
 echo " \\___/|_|   |_|\\_\\"
 echo -e "\n \n"
 
-# Takes a path to the current directory
-get_all_item_names_in_directory () {
-    #TODO
-}
 
 # Takes a list of item names in directory
 print_all_items () {
-    #TODO
+    # TODO: Implement this function
+    for item in "$@"; do
+        echo "$item"
+    done
 }
+
+# Uncomment the following line to display the contents of welcomemessage.txt
+cat welcomemessage.txt
+
+# Main loop
+while true; do
+    # TODO: Add your logic here
+    
+    # Example usage of the functions:
+    current_directory="/home/schreifuchs"
+    items=$(ls "$current_directory")
+    print_all_items $items
+    
+done
+
+
+
+
+
+
