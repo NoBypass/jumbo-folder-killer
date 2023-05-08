9079f238ccbe0e705a2ed792bf1205c9afcc1078
niklas <niklas@sunway.ch>
2023-05-08 09:25:18 +0200

    💡 added some functions to do and shabang

---
+++ src/jfk.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/sh
+
+echo "    | |  ___| |/ /"
+echo " _  | | |_  | ' / "
+echo "| |_| |  _| | . \\ "
+echo " \\___/|_|   |_|\\_\\"
+echo -e "\n \n"
+
+# Takes a path to the current directory
+get_all_item_names_in_directory () {
+    #TODO
+}
+
+# Takes a list of item names in directory
+print_all_items () {
+    #TODO
+}
