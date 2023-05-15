4089769dfd54211c23c053e55decf96f460fa378
NoBypass <nobypass.yt@gmail.com>
2023-05-15 11:11:03 +0200

    Video + Fix

--- src/jfk.sh
+++ src/jfk.sh
@@ -6,33 +6,34 @@ echo "| |_| |  _| | . \\ "
 echo " \\___/|_|   |_|\\_\\"
 echo -e "\n \n"
 
-
 # Takes a list of item names in directory
 print_all_items() {
+  local count=1
   local count=1
   folders=()
   for item in "$@"; do
     size=$(du -sh "$current_directory/$item" | cut -f 1)
     output="${count}) ${item}"
     padding=$((50 - ${#output}))
-    printf "%s %.${padding}s %s\n" "$output" ". . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . " "$size"
-    folders+=("$item")
-    ((count++))
+    if [[ -d "$current_directory/$item" ]]; then
+      printf "%s %.${padding}s %s\n" "$output" ". . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . " "$size"
+      folders+=("$item")
+      ((count++))
+    fi
     if [ "$count" -gt $# ]; then
       break
     fi
   done
 }
 
-
 process_user_input() {
   if [[ $1 == "q" ]]; then
     is_running=0
   elif [[ $input =~ ^[0-9]+$ ]]; then
-    local file_nr=$(($input-1))
-    
+    local file_nr=$(($input - 1))
+
     if [[ $file_nr -lt ${#folders[@]} && $file_nr -ge 0 ]]; then
-      local dir=${folders[file_nr]} 
+      local dir=${folders[file_nr]}
       current_directory+="/$dir"
     else
       echo "Invalid input!!"
@@ -53,9 +54,10 @@ current_directory=$HOME
 is_running=1
 # Main loop
 while [ $is_running -eq 1 ]; do
-    items=$(ls "$current_directory")
-    print_all_items $items
+  items=$(ls "$current_directory")
 
-    read -p "$current_directory >>>" input
-    process_user_input $input
+  print_all_items $items
+
+  read -p "$current_directory >>>" input
+  process_user_input $input
 done
